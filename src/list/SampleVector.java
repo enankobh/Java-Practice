c0017e043e781a145ffa49fb75608f508ed4f6cb
EN334457 <EN334457@D-184002348.wipro.com>
2018-11-19 19:54:02 +0530

    Vector

--- src/list/SampleVector.java
+++ src/list/SampleVector.java
@@ -5,21 +5,21 @@ import java.util.Vector;
 public class SampleVector {
 	
 	public static void main(String[] args) {
-		Vector<String> v = new Vector<String>(2,2);
-		v.ensureCapacity(1); /* setting minimum capacity for the vector */
-		System.out.println(v.capacity());
-		v.add("A");v.add("B");v.add("C");v.add("D");v.add("E");v.add(3, "11");
-		v.trimToSize();
-		System.out.println(v.capacity());
-		System.out.println("---------------------------------------------------------------");
-		System.out.println(v);
-		System.out.println(v.firstElement());
-		System.out.println(v.lastElement());
-		v.setElementAt("22", 1);
-		v.insertElementAt("F", 6);
-		v.removeElementAt(1);
-		System.out.println(v);
-		System.out.println("---------------------------------------------------------------");
+		 Vector<String> v = new Vector<String>(2,2);
+		 v.ensureCapacity(1); /* setting minimum capacity for the vector */
+		 System.out.println(v.capacity());
+		 v.add("A");v.add("B");v.add("C");v.add("D");v.add("E");v.add(3, "11");
+		 v.trimToSize();
+		 System.out.println(v.capacity());
+		 System.out.println("---------------------------------------------------------------");
+		 System.out.println(v);
+		 System.out.println(v.firstElement());
+		 System.out.println(v.lastElement());
+		 v.setElementAt("22", 1);
+		 v.insertElementAt("F", 6);
+		 v.removeElementAt(1);
+		 System.out.println(v);
+		 System.out.println("---------------------------------------------------------------");
 		 Vector vecretain = new Vector(4); 
 		 vecretain.add("A"); 
 	     vecretain.add("11"); 
