6789c2009ec5f20d9a025a149f05fadc2e11a789
EN334457 <EN334457@D-184002348.wipro.com>
2019-01-04 11:55:03 +0530

    Method Overriding main method

--- src/methodoverloading/MethodOverloading.java
+++ src/methodoverloading/MethodOverloading.java
@@ -1,9 +1,24 @@
 package methodoverloading;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodOverloading {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		Child p = new Child();
+		List<String> list = new ArrayList<String>();
+		p.abstractMethod();
+		p.exceptionRuleOneError();
+		p.exceptionRuleOneNormal();
+		p.exceptionRuleTwoError();
+		p.exceptionRuleTwoNormal();
+		p.finalMethod();
+		p.firstMethod();
 		p.invokeParent();
+		p.secondMethod();
+		p.thirdMethod(list);
+		Child.staticMethod();
 	}
 }
